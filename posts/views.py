effd697f0f0a30f39481cf39cff8c3e26fc8bfba
Scott <saseaa37@gmail.com>
2023-03-27 19:30:38 -0600

    First commit

---
+++ posts/views.py
@@ -0,0 +1,28 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Post
+from django.urls import reverse_lazy
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "author"]
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("home")
+    
